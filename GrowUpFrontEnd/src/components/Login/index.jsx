a0dd21e7aabb5881d7a76bec6e09ed58ba453a83
Jonas Felix <jonaspetty@hotmail.com>
2022-11-24 18:39:39 -0300

    adding the login page

--- GrowUpFrontEnd/src/components/Login/index.jsx
+++ GrowUpFrontEnd/src/components/Login/index.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import { useForm } from "react-hook-form"
 
+import "./style.css"
+
 function Login() {
+    const { register , handleSubmit, formState: { errors } } = useForm()
+    const onSubmit = data => console.log(data)
+
     return (
-        <main>
-            
+        <main className='login-main'>
+            <form className='flight-form' onSubmit={handleSubmit(onSubmit)}>
+                <label htmlFor="email" className="form-label">Email</label>
+                <input type="email" name="email" id="email" className='form-control form-control-lg' required
+                {...register('email', {required: "Por favor digite o seu Email"})} />
+
+                <label htmlFor="senha" className="form-label">Senha</label>
+                <input type="password" name="senha" id="senha" className="form-control form-control-lg" required
+                {...register('senha', {required: "Por favor digite a sua Senha"})}/>
+
+                <button type="submit" className='btn btn-primary'>Entrar</button>
+                <Link to="forgotPassword" className='opt-links'>Esqueci minha senha</Link>
+                <br />
+                <Link to="createAccount" className='opt-links'>Ainda não tenho uma conta</Link>
+            </form>
         </main>
     );
 }
